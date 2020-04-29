99b57d918efd2e50e8cea8f20943fa7e81ed1a86
inishaj <inishaj@users.noreply.github.com>
2020-04-29 16:36:02 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -234,7 +234,7 @@ void eliminate(int min)
 {
     for(int i = 0; i < candidate_count; i++)
     {
-        if(candidates[i].votes == min)
+        if((candidates[i].eliminated == false) && (candidates[i].votes == min))//if(candidates[i].votes == min)
         {
             candidates[i].eliminated = true;
         }
